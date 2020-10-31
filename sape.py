6204bb1864e5a6dfe27ff2c72451784a9762527e
Santa <71898556+16bpp-Santa@users.noreply.github.com>
2020-10-31 01:26:13 -0300

    Create sape.py

---
+++ sape.py
@@ -0,0 +1,93 @@
+#coded by Santa
+#all rights reserves 2020
+#GitHub directory
+import turtle
+import time
+t=turtle.Pen()
+t.length = 300.0 
+
+#La S
+t.backward(50)
+t.forward(50)
+t.forward(50)
+t.backward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.backward(50)
+t.left(90)
+t.backward(50)
+t.left(90)
+t.backward(50)
+t.right(90)
+t.backward(50)
+t.right(90)
+t.forward(50)
+
+#La A
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(45)
+t.right(90)
+t.forward(100)
+t.backward(45)
+t.right(90)
+t.forward(45)
+t.backward(45)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+#la P
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(60)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(60)
+t.left(90)
+t.forward(50)
+t.left(180)
+
+t.forward(100)
+t.right(90)
+t.forward(110)
+
+#~La E
+t.forward(50)
+t.backward(50)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.backward(50)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(40)
+
+#color
+
+time.sleep(1)
+turtle.bgcolor("#800080")
+time.sleep(0.5)
+turtle.bgcolor("#18E427")
+time.sleep(0.5)
+turtle.bgcolor("#14FF9B")
+time.sleep(0.5)
+turtle.bgcolor("#FF1458")
+time.sleep(0.5)
+turtle.bgcolor("#D32F1F")
+time.sleep(0.5)
+turtle.bgcolor("#D8FF00")
+time.sleep(0.5)
+turtle.mainloop()
